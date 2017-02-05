b907e65211b66c4177f0ac836cb18c192ab29bfb
Yael Stein <yaelst@gmail.com>
2017-02-05 21:24:37 +0100

    more improvements

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 
 $(document).ready(function() {
-
     var toggle=false;
-
-    console.log('I hear ya');
-
-    console.log((window.document.getElementsByClassName('T-I'))[0]);
-
     $('.T-I').on('click', function() {
-        console.log('compose clicked!!!!');
         setTimeout(function () {
-            console.log('lets seeee');
             $('.OoRYyc').append('<input type="button" class="feinButton" value="Timed Email"></input>');
         }, 1500);
-
         setTimeout(function () {
             $(".feinButton").on('click', function () {
-                console.log('its clicked');
                 if(toggle===false) {
                     toggle=true;
                     $.get(chrome.extension.getURL('dialog.html'), function(data) {
