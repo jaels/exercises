85d44eee1ca321545969dd34d81530cae723c0b5
Yael Stein <yaelst@gmail.com>
2017-02-05 21:47:28 +0100

    with reply

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 $(document).ready(function() {
     var toggle=false;
+
+// This is for when you press Compose to write and email
+
     $('.T-I').on('click', function() {
+        console.log('clicked');
         setTimeout(function () {
             $('.OoRYyc').append('<input type="button" class="feinButton" value="Timed Email"></input>');
         }, 1500);
@@ -21,4 +25,33 @@ $(document).ready(function() {
 
         }, 2000);
     });
+
+    // This is for when you press Reply to write and email
+
+    $(".yX").on('click', function() {
+        setTimeout(function () {
+            $('.ams').on('click', function() {
+                console.log('ams clicked')
+                setTimeout(function () {
+                    $('.OoRYyc').append('<input type="button" class="feinButton" value="Timed Email"></input>');
+                }, 1500);
+                setTimeout(function () {
+                    $(".feinButton").on('click', function () {
+                        if(toggle===false) {
+                            toggle=true;
+                            $.get(chrome.extension.getURL('dialog.html'), function(data) {
+                                $(data).appendTo('.OoRYyc');
+                            });
+                        }
+                        else {
+                            $(".feinPopup").remove();
+                            toggle=false;
+                        }
+                    });
+
+                }, 2000);
+            });
+
+        }, 800);
+    })
 });
