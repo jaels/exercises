fee3788515e3f414f7665ab17a625846dfe541e2
Yael Stein <yaelst@gmail.com>
2017-02-05 20:53:59 +0100

    almost working

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 
-$(document).ready(function () {
-    console.log("Im in!!!!")
-console.log($('.AD'));
+$(document).ready(function() {
+
     var toggle=false;
-    $('.AD').append('<input type="button" class="button" value="Timed email"></input>');
-    $(".button").on('click', function () {
-        if(toggle===false) {
-            toggle=true;
-            $.get(chrome.extension.getURL('dialog.html'), function(data) {
-                $(data).appendTo('.AD');
+
+    console.log('I hear ya');
+
+    console.log((window.document.getElementsByClassName('T-I'))[0]);
+
+    $('.T-I').on('click', function() {
+        console.log('compose clicked!!!!');
+        setTimeout(function () {
+            console.log('lets seeee');
+            $('.OoRYyc').append('<input type="button" class="feinButton" value="Timed Email"></input>');
+        }, 2000);
+
+        setTimeout(function () {
+            $(".feinButton").on('click', function () {
+                console.log('its clicked');
+                if(toggle===false) {
+                    toggle=true;
+                    $.get(chrome.extension.getURL('dialog.html'), function(data) {
+                        $(data).appendTo('.OoRYyc');
+                    });
+                }
+                else {
+                    $(".feinPopup").remove();
+                    toggle=false;
+                }
             });
-        }
-        else {
-            $(".feinPopup").remove();
-            toggle=false;
-        }
+
+        }, 2050);
     });
 });
